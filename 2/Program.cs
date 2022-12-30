ca0fd2d5a89aa2ee0c31d61cf768e519943943a1
German <german.glumov@gmail.com>
2022-12-30 22:18:22 +0300

    дваа

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,13 +1,13 @@
 ﻿
-int Fun(int N, int M)
+int FindingTheAmount(int N, int M)
 {
     //string c = "Ошибка";
     if(M == N) return N;
     //else if(M > N) return c;
-    else return M + Fun(N, M + 1); 
+    else return M + FindingTheAmount(N, M + 1); 
 }
 
 int n = 10;
 int m = 7;
 
-Console.WriteLine(Fun(n, m));
+Console.WriteLine(FindingTheAmount(n, m));
