0d6074a4b7e3a973faa78d9edbb3bd5bbee07e22
German <german.glumov@gmail.com>
2022-12-30 18:14:11 +0300

    два

---
+++ 2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int Fun(int N, int M)
+{
+    //string c = "Ошибка";
+    if(M == N) return N;
+    //else if(M > N) return c;
+    else return M + Fun(N, M + 1); 
+}
+
+int n = 10;
+int m = 7;
+
+Console.WriteLine(Fun(n, m));
