be15a05daa0a527c2d171c01f0f902cceb505b3b
German <german.glumov@gmail.com>
2022-12-30 22:21:53 +0300

    одинн

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Метод, выполняющий подсчёт чисел от n до 1
-string Function(int n)
+string OutputOfNaturalNumbers(int n)
 {
     string falls = "Ошибка";
     if(n == 0)
     {
         return string.Empty;
     }
-    else if(n > 0) return $"{n} " + Function(n - 1);
+    else if(n > 0) return $"{n} " + OutputOfNaturalNumbers(n - 1);
     else return falls;
 }
 
-Console.WriteLine(Function(8));
+Console.WriteLine(OutputOfNaturalNumbers(8));
