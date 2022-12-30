6221c558788e049d6db428f318c00c77a0378377
German <german.glumov@gmail.com>
2022-12-30 18:14:19 +0300

    три

---
+++ 3/Program.cs
@@ -0,0 +1,8 @@
+﻿int function(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return function(m - 1, 1);
+  else return function(m - 1, function(m, n - 1));
+}
+
+Console.WriteLine(function(3,2));
